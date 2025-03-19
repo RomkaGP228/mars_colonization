1eaf61dd44327bb2425c335a3ae7a121b30850b0
geo_prepod_pc <romkagp@yandex.ru>
2025-03-19 23:47:55 +0300

    Готовимся к миссии

--- flask-wtf/main.py
+++ flask-wtf/main.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "kjfqwe23093c.zfa'ewfl2"
-
+app.config['SECRET_KEY'] = "goida"
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
@@ -16,23 +15,14 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('login.html', title='Авторизация', form=form)
 
-@app.route('/success', methods=['GET', 'POST'])
-def success():
-    return render_template('index.html', username="Доступ открыт!")
+@app.route("/<title>")
+@app.route("/index/<title>")
+def index(title):
+    params = {'head1': 'Миссия Колонизация Марса', 'head4': 'И на Марсе будут яблони цвести!'}
+    return render_template("base.html", title=title, **params)
 
-@app.route("/")
-@app.route("/index")
-def index():
-    params = {}
-    return render_template("base.html", title='Заготовка')
 
 if __name__ == '__main__':
-    print('http://127.0.0.1:8080/login')
+    print('http://127.0.0.1:8080/')
     app.run(port=8080, host='127.0.0.1')
