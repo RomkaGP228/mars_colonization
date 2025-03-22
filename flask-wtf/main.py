5cd4a1fcf0dd141ff50364b8c3978f37745bf45e
geo_prepod_pc <romkagp@yandex.ru>
2025-03-22 23:17:02 +0300

    Список профессий

--- flask-wtf/main.py
+++ flask-wtf/main.py
@@ -27,10 +27,17 @@ def index(title):
 
 @app.route("/training/<prof>")
 def training(prof):
-    print(prof)
     return render_template("prof.html", **params, prof=prof)
 
 
+@app.route("/list_prof/<list>")
+def list_prof(list):
+    lst = ['инженер-исследователь', "пилот", "строитель", "экзобиолог", "врач", "инженер по терраформированию",
+           "климатолог", "специалист по радиационной защите", "астрогеолог", "гляциолог", "инженер жизнеобеспечения",
+           "метеоролог", "оператор марсохода", "киберинженер", "штурман", "пилот дронов"]
+    return render_template("list_prof.html", **params, list=list, lst=lst)
+
+
 if __name__ == '__main__':
-    print('http://127.0.0.1:8080/')
+    print('http://127.0.0.1:8080/list_prof/ol')
     app.run(port=8080, host='127.0.0.1')
