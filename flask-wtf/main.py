29ae890c97de1bbeeb1ab16b50244224332145d1
geo_prepod_pc <romkagp@yandex.ru>
2025-03-23 18:39:28 +0300

    Двойная защита

--- flask-wtf/main.py
+++ flask-wtf/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
@@ -10,9 +10,11 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
+    astronaut_id = StringField('Id астронавта', validators=[DataRequired()])
+    astronaut_password = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    captain_id = StringField('Id капитана', validators=[DataRequired()])
+    captain_password = PasswordField('Пароль капитана', validators=[DataRequired()])
+
     submit = SubmitField('Войти')
 
 
@@ -38,7 +40,7 @@ def list_prof(list):
     return render_template("list_prof.html", **params, list=list, lst=lst)
 
 
-@app.route("/answer")
+@app.route("/answer", methods=['GET', 'POST'])
 @app.route("/auto_answer")
 def answer():
     props = {"title": "Анкета", "surname": "Watny", "name": "Mark", "education": "выше среднего",
@@ -47,6 +49,14 @@ def answer():
     return render_template("auto_answer.html", **props, **params)
 
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    props = {'emblem_url': url_for('static', filename='img/MARS-2-7.png')}
+    if form.validate_on_submit():
+        return redirect('/answer')
+    return render_template("login.html", **props, **params, form=form)
+
+
 if __name__ == '__main__':
-    print('http://127.0.0.1:8080/list_prof/ol')
     app.run(port=8080, host='127.0.0.1')
