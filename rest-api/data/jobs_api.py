ef8435aab02f3c1067c0780b46706e9ce3a10eb8
geo_prepod_pc <romkagp@yandex.ru>
2025-05-07 22:09:16 +0300

    Добавление работы, Тестирование POST-запроса

--- rest-api/data/jobs_api.py
+++ rest-api/data/jobs_api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import jsonify
+from flask import jsonify, request, make_response
 from . import db_session
 from .jobs import Jobs
 
@@ -36,3 +36,19 @@ def get_one_jobs(job_id):
 @blueprint.route('/api/jobs/<job_id>')
 def get_job_error(job_id: not int):
     return jsonify({'error': 'Bad request'}), 401
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_news():
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+    elif not all(key in request.json for key in
+                 ['team_leader_id', 'job', 'work_size', 'collaborators', 'is_finished']):
+        return make_response(jsonify({'error': 'Bad request'}), 400)
+    db_sess = db_session.create_session()
+    job = Jobs(team_leader=request.json['team_leader_id'], job=request.json['job'], work_size=int(
+        request.json['work_size']), collaborators=request.json['collaborators'],
+               is_finished=request.json['is_finished'])
+    db_sess.add(job)
+    db_sess.commit()
+    return jsonify({'id': job.id})
--- rest-api/test.py
+++ rest-api/test.py
@@ -1,11 +1,29 @@
 import requests
 
-# получение всех работ
-print(requests.get('http://127.0.0.1:8080/api/jobs').json())
+# # получение всех работ
+# print(requests.get('http://127.0.0.1:8080/api/jobs').json())
+#
+# # получение одной работы
+# print(requests.get('http://127.0.0.1:8080/api/jobs/1').json())
+#
+# print(requests.get('http://localhost:8080/api/jobs/2').json())
+#
+# print(requests.get('http://localhost:8080/api/jobs/abc').json())
 
-# получение одной работы
-print(requests.get('http://127.0.0.1:8080/api/jobs/1').json())
+# проверка post без ошибки
+print(requests.post('http://localhost:8080/api/jobs',
+                    json={'team_leader_id': 1, 'job': 'add new job', 'work_size': 15, 'is_finished': False,
+                          'collaborators': '2, 3'}).json())
 
-print(requests.get('http://localhost:8080/api/jobs/2').json())
+# ошибка в том, что запрос пустой
+print(requests.post('http://localhost:8080/api/jobs', json={}).json())
 
-print(requests.get('http://localhost:8080/api/jobs/abc').json())
+# ошибка в том, что нет поля team leader
+print(requests.post('http://localhost:8080/api/jobs',
+                    json={'job': 'new job', 'work_size': 15, 'is_finished': False,
+                          'collaborators': '2, 3'}).json())
+
+# ошибка в том, что нет поля job
+print(requests.post('http://localhost:8080/api/jobs',
+                    json={'team_leader_id': 1, 'work_size': 15, 'is_finished': False,
+                          'collaborators': '2, 3'}).json())
